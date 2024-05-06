1200dcaf2760167cdc70cb3bf9d481e072b0550a
ViniBraga1 <124733729+ViniBraga1@users.noreply.github.com>
2024-05-06 16:55:40 -0300

    Add files via upload

---
+++ PITCH/Cadastro Motorista.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="wnameth=device-wnameth, initial-scale=1.0">
+    <title>CADASTRO</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+    <h2>CADASTRO DE MOTORISTA</h2>
+    <?php require ("Conexao Motorista.php"); ?>
+    <div class="container">
+        <form action="Create Motorista.php" method="post"> 
+            <div class="form-group">
+                <label>Nome</label>
+                <input type="text" class="form-control" name="nome" placeholder="informe o nome">
+                <label>Email</label>
+                <input type="email" class="form-control" name="email" name="email" placeholder="informe o email">
+                <label>CPF</label>
+                <input type="text" class="form-control" name="cpf" placeholder="informe o cpf">
+                <label>Sexo:</label>
+                <input type="radio" id="M" name="sexo" value="M">
+                <label for="M">Masculino</label>
+                <input type="radio" id="F" name="sexo" value="F">
+                <label for="F">Feminino</label>
+                <input type="radio" id="O" name="sexo" value="O">
+                <label for="O">Outro</label>
+                <label>Endereço</label>
+                <input type="text" class="form-control" name="endereco" placeholder="informe o endereço">
+                <label>Número</label>
+                <input type="text" class="form-control" name="numero" placeholder="informe o número">
+                <label>Complemento</label>
+                <input type="text" class="form-control" name="complemento" placeholder="informe o complemento">
+                <label>Bairro</label>
+                <input type="text" class="form-control" name="bairro" placeholder="informe o bairro">
+                <label>Cidade</label>
+                <input type="text" class="form-control" name="cidade" placeholder="informe a cidade">
+                <label>Número CNH</label>
+                <input type="text" class="form-control" name="cnh" placeholder="informe o número de sua CNH">
+                <label>Data de Vencimento CNH</label>
+                <input type="text" class="form-control" name="datavenc" placeholder="informe a data de vencimento de sua CNH">
+                <label>Categoria CNH:</label>
+                <input type="radio" id="A" name="categoriacnh" value="A">
+                <label for="A">Tipo A</label>
+                <input type="radio" id="B" name="categoriacnh" value="B">
+                <label for="A">Tipo B</label>
+                <input type="radio" id="C" name="categoriacnh" value="C">
+                <label for="A">Tipo C</label>
+                <input type="radio" id="D" name="categoriacnh" value="D">
+                <label for="A">Tipo D</label>
+                <input type="radio" id="E" name="categoriacnh" value="E">
+                <label for="A">Tipo E</label>
+                <label>Código de Motorista</label>
+                <input type="text" class="form-control" name="codigo" placeholder="informe o seu código de motorista">
+                <label>Usuário</label>
+                <input type="text" name="usuario" placeholder="Usuário" required>
+                <label>Senha</label>
+                <input type="password" name="senha" placeholder="Senha" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ PITCH/Tela Site.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MAPA DE ROTAS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mapa Rotas</title>
+    <style>
+        
+        #map {
+            height: 400px;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <h1>Mapa de Rotas</h1>
+
+
+    
+    <div id="map"></div>
+
+   
+    <script>
+        function initMap() {
+            
+            var mymap = L.map('map').setView([-25.363, 131.044], 4);
+
+            
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+            }).addTo(mymap);
+
+           
+            var marker = L.marker([-25.363, 131.044]).addTo(mymap);
+            marker.bindPopup("<b>Sua Localização</b>").openPopup();
+        }
+    </script>
+
+    
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
+   
+    <script>
+        window.onload = initMap;
+    </script>
+</body>
+</html>
+
+
+</body>
+</html>

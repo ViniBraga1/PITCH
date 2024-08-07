b3deaa1fe8e2ad86c4078e073ee36a33a283aba8
ViniBraga1 <124733729+ViniBraga1@users.noreply.github.com>
2024-08-07 20:32:43 -0300

    SvShop

---
+++ SvShop (atualizado)/novo_produto.php
@@ -0,0 +1,24 @@
+<?php
+require ('Conexao.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $descricao = $_POST["descricao"];
+    $preco = $_POST["preco"];
+
+    // Função para inserir um novo registro no banco de dados
+    function inserirRegistro($pdo, $nome, $descricao, $preco) {
+        $sql = "INSERT INTO camisa (nome, descricao, preco) VALUES (:nome, :descricao, :preco)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
+        $stmt->bindParam(':descricao', $descricao, PDO::PARAM_STR);
+        $stmt->bindParam(':preco', $preco, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+}
+if (inserirRegistro($pdo, $nome, $descricao, $preco)) {
+    echo "Podruto inserido com sucesso!<br>" . "<a href='Index.php'>Voltar ao site</a>";
+} else {
+    echo 'Erro ao inserir o registro.';
+}
+?>

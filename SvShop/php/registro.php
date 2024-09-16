17aec0c6e0a96f3aa6e8a33165686fc4bb44aa6e
ViniBraga1 <124733729+ViniBraga1@users.noreply.github.com>
2024-09-16 17:20:46 -0300

    SvShop

---
+++ SvShop/php/registro.php
@@ -0,0 +1,24 @@
+<?php
+require ('Conexao.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    // Função para inserir um novo registro no banco de dados
+    function inserirRegistro($pdo, $username, $email, $senha) {
+        $sql = "INSERT INTO cliente (username, email, senha) VALUES (:username, :email, :senha)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+}
+if (inserirRegistro($pdo, $username, $email, $senha)) {
+    echo "Registro inserido com sucesso!<br>" . "<a href='../views/login.php'>Fazer Login</a>";
+} else {
+    echo 'Erro ao inserir o registro.';
+}
+?>
